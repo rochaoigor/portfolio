2da9e1caeda55d1449ef774f70e8b03b4b85b6b5
rochaoigor <igorsantooos@gmail.com>
2022-09-23 09:44:22 -0300

    atualizações

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,19 @@
 let nome=prompt('Qual o seu nome?')
 let res = alert(`Prazer em te conhecer, ${nome}!`)
+
+ 
+    function clickmenu() {
+        if (items.style.display== "block") {
+         items.style.display = "none"
+        }
+        else {
+         items.style.display = "block"
+        }
+     }
+     function tamanho() {
+         if (window.innerwidth >= 768) {
+             items.style.display = "block"
+         }else {
+             items.style.display = "none"
+         }
+     }
